10f500b17a408b4b7baa9400ad4b399f5fceb1bd
Jason Ye <yeja@oregonstate.edu>
2017-05-04 23:17:23 -0700

    Add files via upload

---
+++ dataDeletion.php
@@ -0,0 +1,55 @@
+<?php
+//load the database configuration file
+include 'dbConfig.php';
+
+if(isset($_POST['importSubmit'])){
+    
+    //validate whether uploaded file is a csv file
+    $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 
+    'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
+    if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'],$csvMimes)){
+        if(is_uploaded_file($_FILES['file']['tmp_name'])){
+            
+            //open uploaded csv file with read only mode
+            $csvFile = fopen($_FILES['file']['tmp_name'], 'r');
+            
+            //skip first line
+            fgetcsv($csvFile);
+            
+            //parse data from csv file line by line
+            while(($line = fgetcsv($csvFile)) !== FALSE){
+                //check whether careerID already exists in database with same careerID
+                $prevQuery = "SELECT careerID FROM Careers WHERE careerID = '".$line[0]."'";
+                $prevResult = $db->query($prevQuery);
+                if($prevResult->num_rows > 0){
+                    //update member data
+                    //$db->query("DELETE FROM Careers, CareerLink USING Careers INNER JOIN CareerLink ON Careers.careerID = CareerLink.careerID WHERE Careers.careerID = '".$line[0]."' ");
+
+                    $queryCareers = "DELETE FROM Careers, CareerLink USING Careers INNER JOIN CareerLink ON Careers.careerID = CareerLink.careerID WHERE Careers.careerID = '".$line[0]."' ";
+                    $db->query($queryCareers);
+                    
+                    //$queryCareerLink = "DELETE FROM CareerLink WHERE careerID = '".$line[0]."'";
+                    //$db->query($queryCareerLink);
+                    
+                    //$queryOpp = "DELETE FROM Opportunity WHERE careerID = '".$line[0]."'";
+
+                }else
+                {
+                    echo "Error!";           
+                }
+            }
+            
+            //close opened csv file
+            fclose($csvFile);
+
+            $qstring = '?status=succ';
+        }else{
+            $qstring = '?status=err';
+        }
+    }else{
+        $qstring = '?status=invalid_file';
+    }
+}
+
+//redirect to the listing page
+header("Location: indexDelete.php".$qstring);
